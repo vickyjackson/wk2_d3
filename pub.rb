e472eb86b4e7823237aa934d7684a2fc742e7b9f
Vicky Jackson <vicky@digitalstuff.net>
2018-07-04 12:00:41 +0100

    Created name and till for pub with tests

--- pub.rb
+++ pub.rb
@@ -1,14 +1,15 @@
 class Pub
 
     # Getters
-
+    attr_reader :name, :till
     # Setters
 
     # Initialize the argument list
-    def initialize()
-
+    def initialize(name, till_value)
+      @name = name
+      @till = till_value
     end
 
     # Class methods
-    
+
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -3,6 +3,19 @@ require_relative("../pub")
 
 class TestPub < Minitest::Test
 
+  def test_name()
+    pub = Pub.new("Queen Vic", 500)
+    assert_equal("Queen Vic", pub.name())
+  end
 
+  def test_till()
+    pub = Pub.new("Queen Vic", 500)
+    assert_equal(500, pub.till())
+  end
+
+  # def test_drinks()
+  #   pub = Pub.new("Queen Vic", 500)
+  #   assert_equal([], pub.drinks())
+  # end
 
 end
